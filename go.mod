c975f400dbfe30de7c850723c857b0ff0bcac9e8
Ramal <ramal@medicalwizard.com.au>
2024-09-29 18:30:11 +0530

    Repo initialized, item struct and Todos slice created

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ramalabe/todo-app
+
+go 1.23.1
---
+++ todo.go
@@ -0,0 +1,12 @@
+package todo
+
+import "time"
+
+type item struct {
+	Task      string
+	Done      bool
+	CreatedAt time.Time
+	CompletedAt time.Time
+}
+
+type Todos []item 
